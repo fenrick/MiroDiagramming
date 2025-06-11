157ebe35171fc36132fd01aa7abb0c8addf4ec6d
Jolyon Suthers <201621+fenrick@users.noreply.github.com>
2025-06-11 14:44:33 +1000

    Add Jest tests and config

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+export default {
+  transform: {
+    '^.+\\.tsx?$': ['babel-jest', { presets: ['@babel/preset-env', '@babel/preset-typescript'] }],
+  },
+  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+};
