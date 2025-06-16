f8b58f3d2558a323fe3f5f24a0d6f99fe9972b49
Jolyon Suthers <201621+fenrick@users.noreply.github.com>
2025-06-16 11:45:54 +1000

    working code

---
+++ src/app.tsx
@@ -0,0 +1,105 @@
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import { useDropzone } from "react-dropzone";
+import { parseCsv } from "./csv-utils";
+import { createMindmap } from "./mindmap";
+
+// UI
+const dropzoneStyles = {
+  display: "flex",
+  height: "100%",
+  flexDirection: "column",
+  justifyContent: "center",
+  textAlign: "center",
+  border: "3px dashed rgba(41, 128, 185, 0.5)",
+  color: "rgba(41, 128, 185, 1.0)",
+  fontWeight: "bold",
+  fontSize: "1.2em",
+} as const;
+
+const App: React.FC = () => {
+  const [files, setFiles] = React.useState<File[]>([]);
+  const dropzone = useDropzone({
+    accept: {
+      "text/csv": [".csv"],
+    },
+    maxFiles: 1,
+    onDrop: (droppedFiles) => {
+      setFiles([droppedFiles[0]]);
+    },
+  });
+
+  const handleCreate = async () => {
+    const failed = [];
+    for (const file of files) {
+      try {
+        const contents = await parseCsv(file);
+        await createMindmap(contents);
+      } catch (e) {
+        failed.push(file);
+        console.error(e);
+      }
+    }
+
+    setFiles([]);
+  };
+
+  const style = React.useMemo(() => {
+    let borderColor = "rgba(41, 128, 185, 0.5)";
+    if (dropzone.isDragAccept) {
+      borderColor = "rgba(41, 128, 185, 1.0)";
+    }
+
+    if (dropzone.isDragReject) {
+      borderColor = "rgba(192, 57, 43,1.0)";
+    }
+    return {
+      ...dropzoneStyles,
+      borderColor,
+    };
+  }, [dropzone.isDragActive, dropzone.isDragReject]);
+
+  return (
+    <div className="dnd-container">
+      <p>Select the CSV file to import and create a mind map</p>
+      <div {...dropzone.getRootProps({ style })}>
+        <input {...dropzone.getInputProps()} />
+        {dropzone.isDragAccept ? (
+          <p className="dnd-text">Drop your CSV file here</p>
+        ) : (
+          <>
+            <div>
+              <button
+                type="button"
+                className="button button-primary button-small"
+              >
+                Select CSV file
+              </button>
+              <p className="dnd-text">Or drop your CSV file here</p>
+            </div>
+          </>
+        )}
+      </div>
+      {files.length > 0 && (
+        <>
+          <ul className="dropped-files">
+            {files.map((file, i) => (
+              <li key={i}>{file.name}</li>
+            ))}
+          </ul>
+
+          <button
+            onClick={handleCreate}
+            className="button button-small button-primary"
+          >
+            Create Mind Map
+          </button>
+        </>
+      )}
+    </div>
+  );
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container!);
+root.render(<App />);
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,31 @@
-import { defineConfig } from 'vite';
-import preact from '@preact/preset-vite';
+import path from "path";
+import fs from "fs";
+import dns from "dns";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/server-options.html#server-host
+dns.setDefaultResultOrder("verbatim");
+
+// make sure vite picks up all html files in root, needed for vite build
+const allHtmlEntries = fs
+  .readdirSync(".")
+  .filter((file) => path.extname(file) === ".html")
+  .reduce((acc: Record<string, string>, file) => {
+    acc[path.basename(file, ".html")] = path.resolve(__dirname, file);
+
+    return acc;
+  }, {});
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    preact(),
-    // Add any required dynamic imports for plugins here
-  ],
   build: {
-    outDir: 'dist',
     rollupOptions: {
-      output: {
-        manualChunks: {
-          elk: ['elkjs/lib/elk.bundled.js'], // Ensure elkjs is properly loaded
-        },
-      },
+      input: allHtmlEntries,
     },
+  },
+  plugins: [react()],
+  server: {
+    port: 3000,
   },
 });
