58324fc62fff53369de11cd8ffaa9a7b0b9027bd
Jolyon Suthers <201621+fenrick@users.noreply.github.com>
2025-07-02 12:44:04 +1000

    style: aligning with miro sizes

--- src/ui/components/legacy/Heading.tsx
+++ src/ui/components/legacy/Heading.tsx
@@ -8,12 +8,43 @@ export type HeadingProps = Readonly<
 export function Heading({
   level = 1,
   className = '',
+  style = {},
   children,
   ...props
 }: HeadingProps): React.JSX.Element {
   const attrs = { className, ...props };
-  if (level === 2) return <h2 {...attrs}>{children}</h2>;
-  if (level === 3) return <h3 {...attrs}>{children}</h3>;
-  if (level === 4) return <h4 {...attrs}>{children}</h4>;
+  if (level === 2)
+    return (
+      <h2
+        style={{
+          fontSize: 'var(--font-sizes-200)',
+          ...(style as React.CSSProperties),
+        }}
+        {...attrs}>
+        {children}
+      </h2>
+    );
+  if (level === 3)
+    return (
+      <h3
+        style={{
+          fontSize: 'var(--font-sizes-175)',
+          ...(style as React.CSSProperties),
+        }}
+        {...attrs}>
+        {children}
+      </h3>
+    );
+  if (level === 4)
+    return (
+      <h4
+        style={{
+          fontSize: 'var(--font-sizes-175)',
+          ...(style as React.CSSProperties),
+        }}
+        {...attrs}>
+        {children}
+      </h4>
+    );
   return <h1 {...attrs}>{children}</h1>;
 }
