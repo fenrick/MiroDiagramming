2782689d83f0cfdb45de0d2742263d39aa7c24be
Jolyon Suthers <201621+fenrick@users.noreply.github.com>
2025-07-02 22:44:22 +1000

    fix: paragraph

--- src/ui/components/legacy/index.ts
+++ src/ui/components/legacy/index.ts
@@ -1,5 +1,5 @@
 export { Select, SelectOption } from './Select';
 export { Text } from './Text';
 export { Icon } from './Icon';
-export { Paragraph } from './Paragraph';
+export { Paragraph } from '../Paragraph';
 export { FormGroup } from './FormGroup';
