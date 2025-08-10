1cf2644f7f173b7099faa8cc4ab04eb40ec02594
Jolyon Suthers <201621+fenrick@users.noreply.github.com>
2025-08-10 16:44:00 +1000

    chore(ci): enable coverage reporting

--- fenrick.miro.client/vitest.config.ts
+++ fenrick.miro.client/vitest.config.ts
@@ -3,6 +3,12 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     environment: 'jsdom',
-    coverage: { provider: 'istanbul', reporter: ['text', 'lcov'] },
+    coverage: {
+      provider: 'istanbul',
+      reporter: ['text', 'lcov'],
+      lines: 70,
+      functions: 70,
+      branches: 60,
+    },
   },
 });
