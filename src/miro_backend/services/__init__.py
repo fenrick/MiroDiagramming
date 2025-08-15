62d66e5e7c44a28c91573c7fc1ada52f74b1f96d
Jolyon Suthers <201621+fenrick@users.noreply.github.com>
2025-08-16 00:11:21 +1000

    feat(logs): add client log ingestion endpoint

--- src/miro_backend/services/__init__.py
+++ src/miro_backend/services/__init__.py
@@ -2,5 +2,6 @@
 
 from .miro_client import MiroClient
 from .repository import Repository
+from .log_repository import LogRepository, get_log_repository
 
-__all__ = ["MiroClient", "Repository"]
+__all__ = ["MiroClient", "Repository", "LogRepository", "get_log_repository"]
