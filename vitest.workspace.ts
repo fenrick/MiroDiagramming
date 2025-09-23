dab90ceb9d0afbe573e4d400dc2684243747e2c0
Jolyon Suthers <201621+fenrick@users.noreply.github.com>
2025-09-23 10:49:04 +1000

    test(config): use test.projects in vitest.config.ts (node/jsdom) and remove workspace files

--- vitest.workspace.ts
+++
@@ -1,8 +0,0 @@
-import { defineWorkspace } from 'vitest/config'
-
-export default defineWorkspace([
-  // Node environment project
-  'vitest.config.node.ts',
-  // jsdom environment project
-  'vitest.config.jsdom.ts',
-])
