8ac6ec82de721cd07e85b693d5859313ab91e8ea
Jolyon Suthers <201621+fenrick@users.noreply.github.com>
2025-09-23 10:34:48 +1000

    test(toast): remove unused waitFor import

--- tests/client/toast.test.tsx
+++ tests/client/toast.test.tsx
@@ -1,5 +1,5 @@
 import { describe, it, expect, vi } from 'vitest'
-import { render, screen, act, waitFor } from '@testing-library/react'
+import { render, screen, act } from '@testing-library/react'
 import React from 'react'
 
 import { ToastContainer, pushToast } from '../../src/ui/components/Toast'
